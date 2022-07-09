24b89bf759d49e7384a0d000fb7973366b47035e
oscarprueba1 <oscar.pt84@gmail.com>
2022-07-09 01:31:56 -0500

    aaa

--- hola-mundo-nestjs/src/products/products.controller.ts
+++ hola-mundo-nestjs/src/products/products.controller.ts
@@ -2,15 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
-
 @Get()
 getHelloInProducts(): string {
           return "Estamos en productos!!!";
 }
 
-
           @Get('hot')
           getSpecialProducts(): string {
                     return "Te vamos a mostrar los productos más calientes!!";
           }
+
 }
